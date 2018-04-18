63dd63a0bcc6bcb55caeb3f3a714172742b7a142
Lyndon Mackay <lyndondmackay@gmail.com>
2018-04-18 12:47:14 +1000

    Initial version with views

---
+++ MVCWebsite/Models/Date.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace MVCWebsite.Models
+{
+    public class Date
+    {
+        public int ID { get; set; }
+
+        public DateTime Time { get; set; }
+    }
+    public class DateDBContext : DbContext
+    {
+        public DateDBContext() { }
+        public DbSet<Date> Dates { get; set; }
+    }
+
+}
---
+++ MVCWebsite/Models/General.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCWebsite.Models
+{
+    public class General
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Country { get; set; }
+
+        public string Comments { get; set; }
+
+        public string Wiki_link { get; set; }
+
+
+    }
+    public class GenDBContext : DbContext {
+        public GenDBContext() { }
+        public DbSet<General> Generals { get; set; }
+    }
+}
