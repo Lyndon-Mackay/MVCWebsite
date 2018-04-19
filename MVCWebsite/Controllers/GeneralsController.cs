517bb73560a7e94e2395f1487173830d363b7e02
Lyndon Mackay <lyndondmackay@gmail.com>
2018-04-19 15:14:31 +1000

    removed unused listings

--- MVCWebsite/Controllers/GeneralsController.cs
+++ MVCWebsite/Controllers/GeneralsController.cs
@@ -1,11 +1,7 @@
 ﻿using MVCWebsite.Models;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq.Dynamic;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 
 namespace MVCWebsite.Controllers
